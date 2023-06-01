d065df97c5fa73c4ab921526d20f8bb379f877f7
Comedian Likt <comedial@et-k6.kzn.21-school.ru>
2023-06-01 17:40:49 +0300

    push back

--- src/s21_vector.cc
+++ src/s21_vector.cc
@@ -29,12 +29,19 @@ public:
   const_reference back();
   iterator data();
 
+  iterator begin();
+  iterator end();
+
   bool empty();
   size_type size();
   size_type max_size();
   void reserve(size_type size);
   size_type capacity();
   void shrink_to_fit();
+
+  void clear();
+  // iterator insert(iterator pos, const_reference value);
+  void push_back(const_reference value);
 
 private:
   T *vector_;
@@ -59,10 +66,10 @@ template <class T> vector<T>::vector(std::initializer_list<T> const &items) {
   capacity_ = size_;
   vector_ = new T[size_]{};
   std::copy(items.begin(), items.end(), vector_);
-  std::cout << "init constructor" << std::endl; // DELETE THIS
-  for (long unsigned int i = 0; i < size_; i++) {
-    std::cout << vector_[i] << std::endl;
-  }
+  // std::cout << "init constructor" << std::endl; // DELETE THIS
+  // for (long unsigned int i = 0; i < size_; i++) {
+  //   std::cout << vector_[i] << std::endl;
+  // }
 }
 
 template <class T> vector<T>::vector(const vector &v) {
@@ -70,10 +77,10 @@ template <class T> vector<T>::vector(const vector &v) {
   capacity_ = size_;
   vector_ = new T[size_]{};
   std::copy(v.vector_, v.vector_ + v.size_, vector_);
-  std::cout << "copy constructor" << std::endl;   // DELETE THIS
-  for (long unsigned int i = 0; i < size_; i++) { // DELETE THIS
-    std::cout << vector_[i] << std::endl;
-  }
+  // std::cout << "copy constructor" << std::endl;   // DELETE THIS
+  // for (long unsigned int i = 0; i < size_; i++) { // DELETE THIS
+  //   std::cout << vector_[i] << std::endl;
+  // }
 }
 
 template <class T> vector<T>::vector(vector &&v) noexcept {
@@ -83,10 +90,10 @@ template <class T> vector<T>::vector(vector &&v) noexcept {
   v.size_ = 0;
   v.capacity_ = 0;
   v.vector_ = nullptr;
-  std::cout << "move constructor" << std::endl;   // DELETE THIS
-  for (long unsigned int i = 0; i < size_; i++) { // DELETE THIS
-    std::cout << vector_[i] << std::endl;
-  }
+  // std::cout << "move constructor" << std::endl;   // DELETE THIS
+  // for (long unsigned int i = 0; i < size_; i++) { // DELETE THIS
+  //   std::cout << vector_[i] << std::endl;
+  // }
 }
 
 template <class T> vector<T>::~vector() {
@@ -105,7 +112,7 @@ template <class T> vector<T> &vector<T>::operator=(vector &&v) noexcept {
     v.capacity_ = 0;
     v.vector_ = nullptr;
   }
-  std::cout << "move operator =" << std::endl; // DELETE THIS
+  // std::cout << "move operator =" << std::endl; // DELETE THIS
   return *this;
 }
 
@@ -119,7 +126,7 @@ template <class T> vector<T> &vector<T>::operator=(const vector &v) {
     size_ = v.size_;
     capacity_ = v.capacity_;
   }
-  std::cout << "copy operator =" << std::endl; // DELETE THIS
+  // std::cout << "copy operator =" << std::endl; // DELETE THIS
   return *this;
 }
 
@@ -145,6 +152,13 @@ template <class T> typename vector<T>::const_reference vector<T>::back() {
 
 template <class T> typename vector<T>::iterator vector<T>::data() {
   return vector_;
+}
+
+template <class T> typename vector<T>::iterator vector<T>::begin() {
+  return vector_;
+}
+template <class T> typename vector<T>::iterator vector<T>::end() {
+  return vector_ + size_;
 }
 
 template <class T> bool vector<T>::empty() { return size_ == 0; }
@@ -177,6 +191,33 @@ template <class T> void vector<T>::shrink_to_fit() {
   }
 }
 
+template <class T> void vector<T>::clear() {
+  size_ = 0;
+  delete[] vector_;
+  vector_ = nullptr;
+}
+
+// template <class T>
+// typename vector<T>::iterator vector<T>::insert(iterator pos,
+//                                                const_reference value) {
+//   if (pos >= begin() && pos <= end()) {
+
+//   }
+
+//   return pos;
+// }
+
+template <class T> void vector<T>::push_back(const_reference value) {
+  if (size_ + 1 <= capacity_) {
+    vector_[size_] = value;
+    size_ += 1;
+  } else {
+    reserve(size_ * 2);
+    vector_[size_] = value;
+    size_ += 1;
+  }
+}
+
 } // namespace s21
 
 int main() {
@@ -195,50 +236,53 @@ int main() {
   // std::cout << "copy" << std::endl;
   // s21::vector<int> cpy_vectr = my_vector;
 
-  s21::vector<int> my_vector(6);
-  std::cout << "Int vector \n Size: " << my_vector.size() << std::endl;
-  std::cout << "Max size: " << my_vector.max_size() << std::endl;
-  std::cout << "Capacity: " << my_vector.capacity() << std::endl;
+  // s21::vector<int> my_vector(6);
+  // std::cout << "Int vector \n Size: " << my_vector.size() << std::endl;
+  // std::cout << "Max size: " << my_vector.max_size() << std::endl;
+  // std::cout << "Capacity: " << my_vector.capacity() << std::endl;
 
-  s21::vector<char> char_vector(7);
-  std::cout << "Char vector \n Size: " << char_vector.size() << std::endl;
-  std::cout << "Max size: " << char_vector.max_size() << std::endl;
-  std::cout << "Capacity: " << char_vector.capacity() << std::endl;
+  // s21::vector<char> char_vector(7);
+  // std::cout << "Char vector \n Size: " << char_vector.size() << std::endl;
+  // std::cout << "Max size: " << char_vector.max_size() << std::endl;
+  // std::cout << "Capacity: " << char_vector.capacity() << std::endl;
 
   s21::vector<std::string> string_vector(7);
   std::cout << "String vector \n Size: " << string_vector.size() << std::endl;
-  std::cout << "Max size: " << string_vector.max_size() << std::endl;
   std::cout << "Capacity: " << string_vector.capacity() << std::endl;
+  string_vector.push_back("last");
+  std::cout << "push_back \n Size: " << string_vector.size() << std::endl;
+  std::cout << "Capacity: " << string_vector.capacity() << std::endl;
+  std::cout << string_vector.at(7) << std::endl;
 
-  s21::vector<int> numbers(6);
-  std::cout << "Current size: " << numbers.size() << std::endl;
-  std::cout << "Current capacity: " << numbers.capacity() << std::endl;
+  // s21::vector<int> numbers(6);
+  // std::cout << "Current size: " << numbers.size() << std::endl;
+  // std::cout << "Current capacity: " << numbers.capacity() << std::endl;
 
-  numbers.reserve(15);
-  std::cout << "reserve. New size: " << numbers.size() << std::endl;
-  std::cout << "New capacity: " << numbers.capacity() << std::endl;
+  // numbers.reserve(15);
+  // std::cout << "reserve. New size: " << numbers.size() << std::endl;
+  // std::cout << "New capacity: " << numbers.capacity() << std::endl;
 
-  numbers[2] = 7;
-  std::cout << "at " << numbers[2] << std::endl;
-  std::cout << "data: " << numbers.data() << std::endl;
+  // numbers[2] = 7;
+  // std::cout << "at " << numbers[2] << std::endl;
+  // std::cout << "data: " << numbers.data() << std::endl;
 
-  numbers[3] = 8;
-  std::cout << "[] " << numbers[3] << std::endl;
+  // numbers[3] = 8;
+  // std::cout << "[] " << numbers[3] << std::endl;
 
-  numbers.shrink_to_fit();
-  std::cout << "shrink_to_fit. New size: " << numbers.size() << std::endl;
-  std::cout << "New capacity: " << numbers.capacity() << std::endl;
+  // numbers.shrink_to_fit();
+  // std::cout << "shrink_to_fit. New size: " << numbers.size() << std::endl;
+  // std::cout << "New capacity: " << numbers.capacity() << std::endl;
 
-  numbers = my_vector;
-  std::cout << "copy. New size: " << numbers.size() << std::endl;
-  std::cout << "New capacity: " << numbers.capacity() << std::endl;
+  // numbers = my_vector;
+  // std::cout << "copy. New size: " << numbers.size() << std::endl;
+  // std::cout << "New capacity: " << numbers.capacity() << std::endl;
 
-  s21::vector<char> letters = {'a', 'b', 'c'};
-  std::cout << "front: " << letters.front() << std::endl;
-  std::cout << "back: " << letters.back() << std::endl;
+  // s21::vector<char> letters = {'a', 'b', 'c'};
+  // std::cout << "front: " << letters.front() << std::endl;
+  // std::cout << "back: " << letters.back() << std::endl;
 
-  s21::vector<std::string> string_vector_two = string_vector;
-  std::cout << "data: " << letters.data() << std::endl;
+  // s21::vector<std::string> string_vector_two = string_vector;
+  // std::cout << "data: " << letters.data() << std::endl;
 
   return 0;
 }
